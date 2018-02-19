41d439f3b2c0ffe0cd5e8635058ee04bd7dd841f
taouladmansour <aouladmansour.t@gmail.com>
2018-02-19 10:49:33 +0100

    annulation changements mult

--- src/mult.cpp
+++ src/mult.cpp
@@ -2,5 +2,17 @@
 #include "../include/add.h"
 
 int mult( int a, int b){
-    return a * b;
+    int c = 0;
+    if (b<0)
+        a*=-1;
+
+    while ( b != 0){
+        c = add(a,c);
+        if(b>0)
+            b--;
+        else
+            b++;
+    }
+
+    return c;
 }
