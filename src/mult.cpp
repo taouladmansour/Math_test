75f292eabf2ce85ab51d495858a86b71e0fa1c58
taouladmansour <aouladmansour.t@gmail.com>
2018-02-19 10:41:16 +0100

    Changements fonction mult

--- src/mult.cpp
+++ src/mult.cpp
@@ -2,17 +2,5 @@
 #include "../include/add.h"
 
 int mult( int a, int b){
-    int c = 0;
-    if (b<0)
-        a*=-1;
-
-    while ( b != 0){
-        c = add(a,c);
-        if(b>0)
-            b--;
-        else
-            b++;
-    }
-
-    return c;
+    return a * b;
 }
